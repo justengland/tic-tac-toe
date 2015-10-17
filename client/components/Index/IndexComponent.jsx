e3f5c970a7c5c1e53b9f8754d9280c5d1b625e1a
jengland <jengland@healthgrades.com>
2015-10-16 18:52:19 -0600

    First Commit

--- client/components/Index/IndexComponent.jsx
+++ client/components/Index/IndexComponent.jsx
@@ -10,7 +10,7 @@ export default class IndexComponent extends React.Component {
 
     return (
       <section>
-        <h2>react-webpack-boilerplate</h2>
+        <h2>Tic Tac Toe</h2>
         <ul ref="indexList" className="index-list">
           {this.props.items.map((item, index) => {
             return (<li key={index}>item {item}</li>);
